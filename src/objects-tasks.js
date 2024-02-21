068cd8f10635132fee78253d8c6a4dfa2b04de17
SPHsofi <Sofia_pash@mail.ru>
2024-02-21 13:41:38 +0300

    refactor: solved task (2-mergeObjects)

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -33,8 +33,14 @@ function shallowCopy(obj) {
  *    mergeObjects([{a: 1, b: 2}, {b: 3, c: 5}]) => {a: 1, b: 5, c: 5}
  *    mergeObjects([]) => {}
  */
-function mergeObjects(/* objects */) {
-  throw new Error('Not implemented');
+function mergeObjects(objects) {
+  const res = {};
+  objects.forEach((elem) => {
+    Object.entries(elem).forEach(([k, v]) => {
+      res[k] = (res[k] || 0) + v;
+    });
+  });
+  return res;
 }
 
 /**

93ec6d074c6c68bf7a890f1ca3577055f821d109
SPHsofi <Sofia_pash@mail.ru>
2024-02-23 18:51:51 +0300

    refactor: solved task (7-makeWord)

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -125,8 +125,17 @@ function makeImmutable(obj) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+function makeWord(lettersObject) {
+  const obj = Object.entries(lettersObject);
+  const arr = [];
+  const res = obj.reduce((acc, [v, idx]) => {
+    const word = acc;
+    idx.forEach((i) => {
+      word[i] = v;
+    });
+    return word;
+  }, arr);
+  return res.join('');
 }
 
 /**
